f21ea18a6403a50dc9e5872dd6d18abb8285030b
hector <odiepus@gmail.com>
2018-02-14 21:11:45 -0600

    so the manpages state that the jiffies for this ver of linux is 1000 but sysconf returns 100. THis explains why i was always off by 10th when working out the seconds. So i used the manpage specs and the math works now.

--- jiffies.c
+++ jiffies.c
@@ -1,3 +1,12 @@
+/*Assignment 2 by Hector R Herrera kec296 
+ * 02/14/2018
+ *
+ * Program will access system jiffies. The function will 
+ * output the jiffies through a function that is implemented into
+ * a kernel module
+ */
+
+
 #include<linux/kernel.h>
 #include<linux/module.h>
 #include<linux/proc_fs.h>
@@ -5,17 +14,22 @@
 #include<linux/timer.h>
 #include<linux/delay.h>
 
+//name the module 
 static char modname[] = "jiffies";
 
+//define the function that will return the jiffies count
 static int my_proc_read(char *buf, char **start, off_t off, int count, int *eof, void *data){
   return sprintf(buf, "%lu\n", jiffies);
 }
 
+
+//create and initiialize the module
 int init_module(void){
   create_proc_read_entry(modname, 0, NULL, my_proc_read, NULL);
   return 0;
 }
 
+//clean up after it returns
 void cleanup_module(void){
   remove_proc_entry(modname, NULL);
 }
--- userSpaceJiff.c
+++ userSpaceJiff.c
@@ -1,11 +1,21 @@
+/* Assingment 2 by Hector Herrera kec296
+ * 02/14/2018
+ * Add a proc module that outputs jiffies
+ * Use a userspace program to confirm jiffies 
+ *  and time in secs correlate.
+ */
+
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 #include<unistd.h>
 
 int main(void){
+  //create file pointers for /proc/jiffies and /proc/uptime
   FILE *fpJiff1;
   FILE *fpJiff2;
+  //uptime will be used to measure accuracy of jiffies
   FILE *fpUpTime;
 
   fpJiff1 = popen("cat /proc/jiffies", "r");
@@ -13,12 +23,15 @@ int main(void){
   fpJiff2 = popen("cat /proc/jiffies", "r");
   fpUpTime = popen("cat /proc/uptime", "r");
 
+  //buffers to hold the output from proc files
   char bufJiff1[4096];
   char bufJiff2[4096];
   char bufUpTime[4096];
 
+  //pointer used to point to discarded input
   char *p;
-
+  
+  //print out the system definition of Hz which should be 1000 but is 100 on my system
   printf("Hz == sysconf(_SC_CLK_TCK) == %lu\n", sysconf(_SC_CLK_TCK));
 
   if(fpJiff1 == NULL || fpJiff2 == NULL || fpUpTime == NULL){
@@ -26,24 +39,33 @@ int main(void){
     exit(2);
   }
 
+  //place proc outputs to buffers
   fgets(bufJiff1, sizeof(bufJiff1), fpJiff1);
   fgets(bufJiff2, sizeof(bufJiff2), fpJiff2);
   fgets(bufUpTime, sizeof(bufUpTime), fpUpTime);
 
+  //extract the value of proc files in specified data type
   unsigned long lJiff1 = strtoul(bufJiff1, &p, 10);
   unsigned long lJiff2 = strtoul(bufJiff2, &p, 10);
   unsigned long lUpTime = strtoul(bufUpTime, &p, 10);
 
-  printf("Start: %lu\n", lJiff1);
-  printf("Start: %lu\n", lJiff2);
+  //print out the initial jiffy capture delay for secs then print out the subsequent capture
+  printf("Start jiffies: %lu\n", lJiff1);
+  printf("Sleep for 2 seconds...\n");
+  printf("Stop jiffies: %lu\n", lJiff2);
+
+  //calculate the difference
   unsigned long dif = lJiff2 - lJiff1;
 
-  printf("Difference: %lu\n", dif/10);
+  printf("Difference: %lu\n", dif);
 
-  printf("Seconds per 100 jiffies: %lu\n", dif/(10 * (unsigned long) sysconf(_SC_CLK_TCK)));
+  printf("Man pages state jiffies for this linux version is 1000. But, sysconf returns 100. I will use manpage specifications. Time elapsed in %lu jiffies is:  %lu secs\n", dif, dif/1000);
 
   printf("Total uptime in jiffies from /proc/jiffies: %lu\n", lJiff2);
-  printf(":Total uptime in seconds from /proc/jiffies%lu\n", dif/(10 * (unsigned long) sysconf(_SC_CLK_TCK)));
+  printf(":Total uptime in seconds from /proc/jiffies%lu\n", dif/1000);
+
+  //there is always a 300 sec difference between uptime and jiffies. this works out to a 5 min
+  //difference. this may have to do with the 5 min overflow that is designed into jiffies.
   printf("Total uptime in seconds from /proc/uptime: %lu\n", lUpTime);
 
   return 0;
